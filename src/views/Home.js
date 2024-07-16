8f2a0ac6156210e48b22aa09766a257cbb7d17c0
LicetLemus <161054695+LicetLemus@users.noreply.github.com>
2024-07-16 15:49:39 -0500

    styles: import header and footer styles in home.css and chatGrupal.css

--- src/views/Home.js
+++ src/views/Home.js
@@ -3,49 +3,45 @@ import { data } from "../data/dataset.js";
 import { Header } from "../components/Header.js";
 import { Footer } from "../components/Footer.js";
 import { Cards } from "../components/Cards.js";
-import { filterData, sortData, } from "../lib/dataFunctions.js";
-
-const rootElement = document.getElementById("root");
-rootElement.insertAdjacentElement("beforebegin", Header());
-rootElement.insertAdjacentElement("afterend", Footer());
+import { filterData, sortData } from "../lib/dataFunctions.js";
 
 export function Home() {
-  return Cards(data);
-} 
+  const rootElement = document.getElementById("root");
+  rootElement.insertAdjacentElement("beforebegin", Header());
+  rootElement.insertAdjacentElement("afterend", Footer());
 
-function resetSelectIndex(...selectElements) {
-  selectElements.forEach((selectElement) => {
-    selectElement.selectedIndex = 0;
-  });
-}
-
-function applyFilterAndSort(activeFilter, inactiveFilter, orderSelect) {
-  let filteredData = data;
-  if (activeFilter.value) {
-    filteredData = filterData(
-      filteredData,
-      activeFilter.name,
-      activeFilter.value
-    );
-    resetSelectIndex(inactiveFilter);
+  function resetSelectIndex(...selectElements) {
+    selectElements.forEach((selectElement) => {
+      selectElement.selectedIndex = 0;
+    });
   }
 
-  if (orderSelect.value) {
-    filteredData = sortData(filteredData, "name", orderSelect.value);
+  function applyFilterAndSort(activeFilter, inactiveFilter, orderSelect) {
+    let filteredData = data;
+    if (activeFilter.value) {
+      filteredData = filterData(
+        filteredData,
+        activeFilter.name,
+        activeFilter.value
+      );
+      resetSelectIndex(inactiveFilter);
+    }
+
+    if (orderSelect.value) {
+      filteredData = sortData(filteredData, "name", orderSelect.value);
+    }
+
+    rootElement.innerHTML = "";
+    rootElement.appendChild(Cards(filteredData));
   }
 
-  rootElement.innerHTML = "";
-  rootElement.appendChild(Cards(filteredData));
-}
+  function handleOrder(data, orderSelect) {
+    let orderedData = data;
+    orderedData = sortData(orderedData, "name", orderSelect.value);
+    rootElement.innerHTML = "";
+    rootElement.appendChild(Cards(orderedData));
+  }
 
-function handleOrder(data, orderSelect) {
-  let orderedData = data;
-  orderedData = sortData(orderedData, "name", orderSelect.value);
-  rootElement.innerHTML = "";
-  rootElement.appendChild(Cards(orderedData));
-}
-
-document.addEventListener("DOMContentLoaded", () => {
   const filterSelectType = document.querySelector("#type-select");
   const filterSelectApplication = document.querySelector(
     "#applicationField-select"
@@ -97,5 +93,6 @@ document.addEventListener("DOMContentLoaded", () => {
     h3Elements[1].innerHTML = "Lenguaje De Programación Más Antiguo";
     h3Elements[2].innerHTML = "Lenguaje De Programación Más Actual";
   });
-});
 
+  return Cards(data);
+}
