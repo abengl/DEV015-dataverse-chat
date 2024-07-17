977fc1be8416e20e5d4a7c315b4b98e6c39a6a52
Aben <126703861+abengl@users.noreply.github.com>
2024-07-17 11:06:26 -0500

    fix: update introduction on mobile #18

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ export function Footer() {
     <h4 class="footer__logo__title">TechGenius</h4>
   </div>
   <div class="footer__text">
-    <p class="footer__text__name>Desarrollado por <a class="footer__text__link" href="https://github.com/LicetLemus">Licet Lemus</a> y <a class="footer__text__link" href="https://github.com/abengl">Alessandra Godoy</a></p>
+    <p class="footer__text__name">Desarrollado por <a class="footer__text__link" href="https://github.com/LicetLemus">Licet Lemus</a> y <a class="footer__text__link" href="https://github.com/abengl">Alessandra Godoy</a></p>
     <p class="footer__text__copy">&copy; Copyright 2024</p>
   </div>
   <div class="footer__block"></div>
