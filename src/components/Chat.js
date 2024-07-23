3fd750555d589127ef997e38911ab46458dd3e1f
LicetLemus <161054695+LicetLemus@users.noreply.github.com>
2024-07-23 17:29:55 -0500

    feature: individual chat component and component styles #20

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -13,6 +13,42 @@ export const Chat = (props) => {
               <h4 class="chat__details__status">En linea</h4>
             </div>
         </div>
+        <div class="overflow">
+          <div class="chat__send">
+            <div class="chat__message__text">
+              Hola, ¿podrías indicarme cuál es el principal uso en el mundo de desarrollo web?
+            </div>
+            <img class="chat__message__image" src="../assets/icons/user.svg" alt="chat icon" itemprop="image"/>
+          </div>
+          <div class="chat__reply">
+            <img class="chat__message__image" src="${itemData.extraInfo.logoUrl}" alt="chat icon" itemprop="image"/>
+            <div class="chat__message__text">
+              ${itemData.description}
+            </div>
+          </div>
+          <div class="chat__send">
+            <div class="chat__message__text">
+              ¿Qué otro dato importante me podrías dar?
+            </div>
+            <img class="chat__message__image" src="../assets/icons/user.svg" alt="chat icon" itemprop="image"/>
+          </div>
+          <div class="chat__reply">
+            <img class="chat__message__image" src="${itemData.extraInfo.logoUrl}" alt="chat icon" itemprop="image"/>
+            <div class="chat__message__text">
+              ${itemData.description}
+            </div>
+          </div>
+        </div>
+        <div class="chat__input">
+          <input 
+            type="text" 
+            placeholder="Escribe tu mensaje..."
+            class="chat__input__field"/>
+          <button class="chat__input__button">
+            Enviar
+          </button>
+        </div>
     `;
   return containerChat;
 };
+
