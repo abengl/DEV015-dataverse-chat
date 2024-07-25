7e209cfb5db8ffed4d2770742adb4b505577f3ae
LicetLemus <161054695+LicetLemus@users.noreply.github.com>
2024-07-25 15:30:29 -0500

    update: view styles

--- src/views/ApiKey.js
+++ src/views/ApiKey.js
@@ -19,12 +19,16 @@ export function ApiKey() {
     <p class="container__description">Conversa con la tecnología de programación preferida. 
     Ingresa tu API KEY y descubre mucho más sobre la tecnología.</p>
     <label for="apikey" class="container__label">API KEY</label>
-    <input type="text" class="container__input" id="apikey" placeholder="Ingresa tu API KEY" />
+    <input type="text" class="container__input" id="apikey" placeholder="Ingresa tu API KEY" required readonly/>
     <div class="container__button">
       <button id="button__clear">Borrar</button>
       <button id="button__save">Guardar</button>
     </div>
     <button id="container__back">Volver a Inicio</button>
+    <div class="container__link">
+      <p class="container__link__text">¿Aún no tienes tu ApiKey?</p>
+      <a class="container__link__open" href="https://openai.com/" target="_blank">Genera tu ApiKey</a>
+    </div>
     `;
 
   loadStylesheet("./styles/apiKey.css");
