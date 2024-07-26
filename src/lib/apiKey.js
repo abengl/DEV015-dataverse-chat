6ddcd2390e09b73fadf1eb65755aa581c9f98138
Aben <126703861+abengl@users.noreply.github.com>
2024-07-25 23:05:46 -0500

    feature: apikey set and get functions #48

--- src/lib/apiKey.js
+++ src/lib/apiKey.js
@@ -1,44 +1,12 @@
 export const getApiKey = () => {
-    // Implementa el código para obtener la API KEY desde Local Storage
+  // Implementa el código para obtener la API KEY desde Local Storage
+  const APIKEY = localStorage.getItem("APIKEY");
+  console.log("get key: ", APIKEY);
+  return APIKEY;
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- };
- 
- export const setApiKey = (key) => {
-   // Implementa el código para guardar la API KEY en Local Storage
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-   
- };
+export const setApiKey = (key) => {
+  // Implementa el código para guardar la API KEY en Local Storage
+  console.log("set key: ", key);
+  localStorage.setItem("APIKEY", key);
+};
