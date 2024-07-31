6222c71bab75d54675ecc8f5cb00812d0009d3c3
Aben <126703861+abengl@users.noreply.github.com>
2024-07-31 15:52:04 -0500

    feature: openAI function and integration on chat component

--- src/lib/openAIApi.js
+++ src/lib/openAIApi.js
@@ -1,6 +1,37 @@
-// Importa la función para obtener la API KEY desde apiKey.js
-import { getApiKey } from './apiKey.js';
+import { getApiKey } from "./apiKey.js";
 
 export const communicateWithOpenAI = (messages) => {
-   //Aquí es donde debes implementar la petición con fetch o axios
+  const APIKEY = getApiKey();
+  const url = "https://api.openai.com/v1/chat/completions";
+
+  if (!APIKEY) {
+    return Promise.reject(
+      new Error(
+        "API KEY no ha sido encontrada, por favor ingrese una API KEY desde el botón Api."
+      )
+    );
+  }
+  return fetch(url, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${APIKEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      model: "gpt-4",
+      messages: messages,
+    }),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(
+          `Error en la petición a OpenAI: ${response.statusText}`
+        );
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      console.error(`Error en la petición a OpenAI: ${error}`);
+      throw error;
+    });
 };
