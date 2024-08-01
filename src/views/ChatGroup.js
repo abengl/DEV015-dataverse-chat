0e69ac6c62d08e3360b6d45c29b53f137bc5b783
LicetLemus <161054695+LicetLemus@users.noreply.github.com>
2024-08-01 16:59:18 -0500

    feature: add technology list in chatGroup HTML/CSS and display:none for panel button in view #61

--- src/views/ChatGroup.js
+++ src/views/ChatGroup.js
@@ -1,4 +1,5 @@
 import { Nav } from "../components/Nav.js";
+import { data } from "../data/dataset.js";
 //import { navigateTo } from "../router.js";
 
 /**
@@ -57,36 +58,26 @@ export function ChatGroup() {
 
   // vista lateral con usuarios
 
+  const ulTechnologies = document.createElement("ul");
+  ulTechnologies.classList.add("technologies");
+  mainElement.appendChild(ulTechnologies);
 
+  data.forEach((objeto) => {
+    const liTechnologies = document.createElement("li");
+    liTechnologies.classList.add("technologies__info");
 
+    liTechnologies.innerHTML = `
+      <div class="technologies__info__lateral">
+        <img id="info__lateral__image" src="${objeto.imageUrl}" alt="${objeto.name}"/>
+        <div id="info__lateral__text">
+          <h3 id="lateral__text__name">${objeto.name}</h3>
+          <p id="lateral__text__description">${objeto.shortDescription}</p>
+        </div>
+      </div>
+    `;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-
-
-
-
-
-
-
-
-
-
-
-  
-
+    ulTechnologies.appendChild(liTechnologies);
+  })
 
   //getElementsAndEvents is a function that sets up the necessary events and behaviors for the elements within the component's view
   const getElementsAndEvents = () => {};
